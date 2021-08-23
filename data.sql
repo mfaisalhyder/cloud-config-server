be5b8bbc67c8c391d07e9b6f507ae338501bc6cd
Muhammad Faisal Hyder <mfaisalhyder@gmail.com>
2021-08-23 22:05:10 +0400

    cloud-config-server

--- data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Book(ID, ISBN, Name, Description, Author, Type, Price, Status) VALUES (-1, '978-3-16-148410-1', 'It Ends With US', 'About domestic violence', 'Collin Hoover', 'FICTIONAL', 40.00, 3)
-INSERT INTO Book(ID, ISBN, Name, Description, Author, Type, Price, Status) VALUES (-2, '978-3-16-148410-2', 'Harry Potter', 'About magic', 'J.K. Rowling', 'FANTASY', 40.00, 2)
-INSERT INTO Book(ID, ISBN, Name, Description, Author, Type, Price, Status) VALUES (-3, '978-3-16-148410-3', 'Dragon Ball Z', 'Based on worlds best anime', 'Akira Toriyama', 'COMIC', 40.00, 1)
-INSERT INTO Book(ID, ISBN, Name, Description, Author, Type, Price, Status) VALUES (-4, '978-3-16-148410-4', 'New Life', 'Based on our life', 'Ellen Musk', 'MYSTERY', 40.00, 3)
